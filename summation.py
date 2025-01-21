9cec6fb17680595c874401a6e9d26bdddeaf94a8
Logan Reine <log.a.of.n@gmail.com>
2025-01-21 16:51:58 -0500

    refactors

--- summation.py
+++ summation.py
@@ -1,19 +1,9 @@
 from sympy import symbols, summation
 
-from sympy import symbols, summation
-
-
-
 # Define the variable and expression for the summation
-
 k, n = symbols('k n')
-
 expression = 4*k + 1
 
-
-
 # Calculate the summation
-
 summation_expr = summation(expression, (k, 0, n))
-
 print(summation_expr.simplify())
